a170ca3e0be51d9f5b1241aa1bc62686f645fcac
Séb Duval <sebastienduvalbcn@gmail.com>
2021-12-10 17:19:15 +0100

    First work on Amazonas page

--- pages/home-amazonas.php
+++ pages/home-amazonas.php
@@ -1,53 +1,54 @@
 <?php include '../includes/header.php'; ?>
 
-<div id="home_banner"></div>
+<section class="home-internal-title">
+    <h3>Perú</h3>
+    <h2>Amazonas</h2>
+    <h3>Barcelona</h3>
+</section>
 
-<div style="text-align: center; color: #54595F; font-size: 1.4em;  padding: 20px; text-transform: uppercase;">
-    <h3 style="margin: 0; font-family: FuturaStd-Light, Sans-serif; font-weight: 100;">Perú</h3>
-    <h2 style="margin: 0; font-family: FuturaLTPro-Book, Sans-serif; font-weight: 300;">Amazonas</h2> 
-    <h3 style="margin: 0; font-family: FuturaStd-Light, Sans-serif; font-weight: 100;">Barcelona</h3>
-</div>
+<section class="home-internal-description">
+    <p>Mi amor por Barcelona, el modernismo y la naturaleza, fusionado con “los trazos fluidos y sinceros” de los niños
+        de la amazonía peruana, al pintarse sus tatuajes con sus propias manos, dieron vida a esta inspiración, que vá
+        acompañada de las iniciales de Dalí y Gaudí (mis padres del arte) y Máximo Laura, artista textil peruano, al
+        cual admiro por su forma de trasmitir la Cosmovisión Andina.</p>
+</section>
 
-<div style="padding: 20px;">
-    <p style="font-family: FuturaLTPro-Book, sans-serif; font-size: 1em; line-height: 1.7em; text-align: center; color: #54595F;">Mi amor por Barcelona, el modernismo y la naturaleza, fusionado con “los trazos fluidos y sinceros” de los niños de la amazonía peruana, al pintarse sus tatuajes con sus propias manos, dieron vida a esta inspiración, que vá acompañada de las iniciales de Dalí y Gaudí (mis padres del arte) y Máximo Laura, artista textil peruano, al cual admiro por su forma de trasmitir la Cosmovisión Andina.</p>
-</div>
-
-<div style="background-image: url(../images/amazonas1_bis.jpg); margin: 20px; height: 394px; background-size: cover; background-repeat: no-repeat; display: flex; justify-content: center; align-items: center;">
+<section style="background-image: url(../images/amazonas1_bis.jpg);" class="home-internal-photo">
     <img src="../images/amazonas2.jpg" alt="" style="width: 200px;">
-</div>
+</section>
 
-<div style="background-image: url(../images/amazonas3.png); margin: 20px; height: 394px; background-size: cover; background-repeat: no-repeat; display: flex; justify-content: center; align-items: center;">
-</div>
+<section style="background-image: url(../images/amazonas3.png);" class="home-internal-photo">
+</section>
 
-<div style="background-image: url(../images/amazonas6.jpg); margin: 20px; height: 394px; background-size: cover; background-repeat: no-repeat; display: flex; justify-content: center; align-items: center;">
-</div>
+<section style="background-image: url(../images/amazonas6.jpg);" class="home-internal-photo">
+</section>
 
-<div style="background-image: url(../images/amazonas9.jpg); margin: 20px; height: 394px; background-size: cover; background-repeat: no-repeat; display: flex; justify-content: center; align-items: center;">
-</div>
+<section style="background-image: url(../images/amazonas9.jpg);" class="home-internal-photo">
+</section>
 
-<div style="background-image: url(../images/amazonas4.jpg); margin: 20px; height: 394px; background-size: cover; background-repeat: no-repeat; display: flex; justify-content: center; align-items: center;">
-</div>
+<section style="background-image: url(../images/amazonas4.jpg);" class="home-internal-photo">
+</section>
 
-<div style="background-image: url(../images/amazonas8.jpg); margin: 20px; height: 394px; background-size: cover; background-repeat: no-repeat; display: flex; justify-content: center; align-items: center;">
-</div>
+<section style="background-image: url(../images/amazonas8.jpg);" class="home-internal-photo">
+</section>
 
-<div style="background-image: url(../images/amazonas11.jpg); margin: 20px; height: 394px; background-size: cover; background-repeat: no-repeat; display: flex; justify-content: center; align-items: center;">
-</div>
+<section style="background-image: url(../images/amazonas11.jpg);" class="home-internal-photo">
+</section>
 
-<div style="background-image: url(../images/amazonas10.jpg); margin: 20px; height: 394px; background-size: cover; background-repeat: no-repeat; display: flex; justify-content: center; align-items: center;">
-</div>
+<section style="background-image: url(../images/amazonas10.jpg);" class="home-internal-photo">
+</section>
 
-<div style="background-image: url(../images/amazonas7.jpg); margin: 20px; height: 394px; background-size: cover; background-repeat: no-repeat; display: flex; justify-content: center; align-items: center;">
-</div>
+<section style="background-image: url(../images/amazonas7.jpg);" class="home-internal-photo">
+</section>
 
-<div style="background-image: url(../images/amazonas12.jpg); margin: 20px; height: 394px; background-size: cover; background-repeat: no-repeat; display: flex; justify-content: center; align-items: center;">
-</div>
+<section style="background-image: url(../images/amazonas12.jpg);" class="home-internal-photo">
+</section>
 
 <p>
     <img src="../images/logos4-e1614154351421.png" alt="" style="width: 200px;">
 </p>
 
-logo + text 
+logo + text
 
 pagination
 
