83ca9c4494e4050518c8b6c648b5ade656192c7c
Séb Duval <sebastienduvalbcn@gmail.com>
2021-12-27 22:09:02 +0100

    Update home internal page style

--- pages/home-amazonas.php
+++ pages/home-amazonas.php
@@ -1,55 +1,68 @@
 <?php include '../includes/header.php'; ?>
 
-<section class="home-internal-title">
-    <h3>Perú</h3>
-    <h2>Amazonas</h2>
-    <h3>Barcelona</h3>
-</section>
+<div class="home-internal">
 
-<section class="home-internal-description">
-    <p>Mi amor por Barcelona, el modernismo y la naturaleza, fusionado con “los trazos fluidos y sinceros” de los niños
-        de la amazonía peruana, al pintarse sus tatuajes con sus propias manos, dieron vida a esta inspiración, que vá
-        acompañada de las iniciales de Dalí y Gaudí (mis padres del arte) y Máximo Laura, artista textil peruano, al
-        cual admiro por su forma de trasmitir la Cosmovisión Andina.</p>
-</section>
+    <div class="home-internal-title">
+        <h2>Perú</h2>
+        <h1>Amazonas</h1>
+        <h2>Barcelona</h2>
+    </div>
 
-<section style="background-image: url(../images/amazonas1_bis.jpg);" class="home-internal-photo">
-    <img src="../images/amazonas2.jpg" alt="" style="width: 200px;">
-</section>
+    <div class="home-internal-description">
+        <p>Mi amor por Barcelona, el modernismo y la naturaleza, fusionado con <strong>“los trazos fluidos y
+                sinceros”</strong> de los niños
+            de la amazonía peruana, al pintarse sus tatuajes con sus propias manos, dieron vida a esta inspiración, que
+            vá
+            acompañada de las iniciales de Dalí y Gaudí (mis padres del arte) y Máximo Laura, artista textil peruano, al
+            cual admiro por su forma de trasmitir la Cosmovisión Andina.</p>
+    </div>
 
-<section style="background-image: url(../images/amazonas3.png);" class="home-internal-photo">
-</section>
+    <div style="background-image: url(../images/amazonas1_bis.jpg); background-repeat: no-repeat;">
+        <img src="../images/amazonas2.jpg" alt=""
+            style="max-width: 625px; min-height: 745px; -webkit-transform: scaleX(-1); transform: scaleX(-1);}">
+    </div>
 
-<section style="background-image: url(../images/amazonas6.jpg);" class="home-internal-photo">
-</section>
+    <div class=row>
+        <div style="background-image: url(../images/amazonas3.png);" class="card">
+        </div>
 
-<section style="background-image: url(../images/amazonas9.jpg);" class="home-internal-photo">
-</section>
+        <div style="background-image: url(../images/amazonas6.jpg);" class="card">
+        </div>
+    </div>
 
-<section style="background-image: url(../images/amazonas4.jpg);" class="home-internal-photo">
-</section>
+    <div class=row>
+        <div style="background-image: url(../images/amazonas9.jpg);" class="card">
+        </div>
 
-<section style="background-image: url(../images/amazonas8.jpg);" class="home-internal-photo">
-</section>
+        <div style="background-image: url(../images/amazonas4.jpg);" class="card">
+        </div>
+    </div>
 
-<section style="background-image: url(../images/amazonas11.jpg);" class="home-internal-photo">
-</section>
+    <div class=row>
+        <div style="background-image: url(../images/amazonas8.jpg);" class="card">
+        </div>
+    </div>
 
-<section style="background-image: url(../images/amazonas10.jpg);" class="home-internal-photo">
-</section>
+    <div style="background-image: url(../images/amazonas11.jpg);" class="card">
+    </div>
 
-<section style="background-image: url(../images/amazonas7.jpg);" class="home-internal-photo">
-</section>
+    <div style="background-image: url(../images/amazonas10.jpg);" class="card">
+    </div>
 
-<section style="background-image: url(../images/amazonas12.jpg);" class="home-internal-photo">
-</section>
+    <div style="background-image: url(../images/amazonas7.jpg);" class="card">
+    </div>
 
-<p>
-    <img src="../images/logos4-e1614154351421.png" alt="" style="width: 200px;">
-</p>
+    <div style="background-image: url(../images/amazonas12.jpg);" class="card">
+    </div>
 
-logo + text
+    <p>
+        <img src="../images/logos4-e1614154351421.png" alt="" style="width: 200px;">
+    </p>
 
-pagination
+    logo + text
+
+    pagination
+
+</div>
 
 <?php include '../includes/footer.php'; ?>
