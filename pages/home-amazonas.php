f1a034aa8565d41fd61c3260c80fb6860d0a0016
Séb Duval <sebastienduvalbcn@gmail.com>
2021-12-29 21:22:37 +0100

    Add a new container to patterns pages

--- pages/home-amazonas.php
+++ pages/home-amazonas.php
@@ -1,68 +1,61 @@
 <?php include '../includes/header.php'; ?>
 
-<div class="home-internal">
-
-    <div class="home-internal-title">
-        <h2>Perú</h2>
-        <h1>Amazonas</h1>
-        <h2>Barcelona</h2>
-    </div>
-
-    <div class="home-internal-description">
-        <p>Mi amor por Barcelona, el modernismo y la naturaleza, fusionado con <strong>“los trazos fluidos y
-                sinceros”</strong> de los niños
-            de la amazonía peruana, al pintarse sus tatuajes con sus propias manos, dieron vida a esta inspiración, que
-            vá
-            acompañada de las iniciales de Dalí y Gaudí (mis padres del arte) y Máximo Laura, artista textil peruano, al
-            cual admiro por su forma de trasmitir la Cosmovisión Andina.</p>
-    </div>
-
-    <div style="background-image: url(../images/amazonas1_bis.jpg); background-repeat: no-repeat;">
-        <img src="../images/amazonas2.jpg" alt=""
-            style="max-width: 625px; min-height: 745px; -webkit-transform: scaleX(-1); transform: scaleX(-1);}">
-    </div>
-
-    <div class=row>
-        <div style="background-image: url(../images/amazonas3.png);" class="card">
+<div class="patterns">
+    <div class="container">
+        <div class="title">
+            <h2>Perú</h2>
+            <h1>Amazonas</h1>
+            <h2>Barcelona</h2>
+        </div>
+        <div class="description">
+            <p>Mi amor por Barcelona, el modernismo y la naturaleza, fusionado con <strong>“los trazos fluidos y
+                    sinceros”</strong> de los niños de la amazonía peruana, al pintarse sus tatuajes con sus propias
+                manos, dieron vida a esta inspiración, que vá acompañada de las iniciales de Dalí y Gaudí (mis padres
+                del arte) y Máximo Laura, artista textil peruano, al cual admiro por su forma de trasmitir la
+                Cosmovisión Andina.</p>
         </div>
 
-        <div style="background-image: url(../images/amazonas6.jpg);" class="card">
-        </div>
-    </div>
-
-    <div class=row>
-        <div style="background-image: url(../images/amazonas9.jpg);" class="card">
+        <div style="background-image: url(../images/amazonas1_bis.jpg);" id="amazonas1_bis">
+            <img src="../images/amazonas2.jpg" alt="amazonas2"
+                style="max-width: 625px; min-height: 745px; -webkit-transform: scaleX(-1); transform: scaleX(-1);">
         </div>
 
-        <div style="background-image: url(../images/amazonas4.jpg);" class="card">
+        <div class=row>
+            <div style="background-image: url(../images/amazonas3.png);" class="card"></div>
+            <div style="background-image: url(../images/amazonas6.jpg);" class="card"></div>
         </div>
-    </div>
 
-    <div class=row>
-        <div style="background-image: url(../images/amazonas8.jpg);" class="card">
+        <div class=row>
+            <div style="background-image: url(../images/amazonas9.jpg);" class="card"></div>
+            <div style="background-image: url(../images/amazonas4.jpg);" class="card"></div>
         </div>
+
+        <div class=row>
+            <div style="background-image: url(../images/amazonas8.jpg);"></div>
+        </div>
+
+        <div class=row>
+            <div style="background-image: url(../images/amazonas11.jpg);"></div>
+        </div>
+
+        <div class=row>
+            <div style="background-image: url(../images/amazonas10.jpg);" class="card"></div>
+            <div style="background-image: url(../images/amazonas7.jpg);" class="card"></div>
+        </div>
+
+        <div class=row>
+            <div style="background-image: url(../images/amazonas12.jpg);"></div>
+        </div>
+
+        <!-- <p>
+            <img src="../images/logos4-e1614154351421.png" alt="" style="width: 200px;">
+        </p> -->
+
+        <!-- logo + text
+
+        pagination -->
+
     </div>
-
-    <div style="background-image: url(../images/amazonas11.jpg);" class="card">
-    </div>
-
-    <div style="background-image: url(../images/amazonas10.jpg);" class="card">
-    </div>
-
-    <div style="background-image: url(../images/amazonas7.jpg);" class="card">
-    </div>
-
-    <div style="background-image: url(../images/amazonas12.jpg);" class="card">
-    </div>
-
-    <p>
-        <img src="../images/logos4-e1614154351421.png" alt="" style="width: 200px;">
-    </p>
-
-    logo + text
-
-    pagination
-
 </div>
 
 <?php include '../includes/footer.php'; ?>
