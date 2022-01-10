5e7792afffc58eabf4cea066c75ca7cc94084697
Séb Duval <sebastienduvalbcn@gmail.com>
2022-01-10 22:42:47 +0100

    Work on home naturaleza page

--- pages/home-naturaleza.php
+++ pages/home-naturaleza.php
@@ -1,5 +1,66 @@
 <?php include '../includes/header.php'; ?>
 
-<h1>Home Naturaleza</h1>
+<div class="patterns">
+    <div class="container">
+        <div class="title">
+            <h2>Bondadosa</h2>
+            <h1>Naturaleza</h1>
+            <h2>Cuarentena</h2>
+        </div>
+        <div class="description">
+            <p>Momentos oscuros… muy oscuros!! Y momentos de claridad también. Pero la naturaleza siempre estuvo y está
+                presente a nuestro alrededor sosteniéndonos, sólo basta parar un poquito, respirar y verla con ojos
+                nuevos, con ojos verdaderos. “Con Ojos del Alma”.
+                Este es un homenaje a esa sensación que sentí al apreciarla, dándome calma me decía que las cosas iban a
+                estar bien. Que esto es un proceso para interiorizar y que ella misma se estaba limpiando para nosotros.
+            </p>
+        </div>
+
+        <div class=row>
+            <div style="background-image: url(../images/naturaleza7.jpg);" id="naturaleza7" class="card-large">
+            </div>
+        </div>
+
+        <div class="row">
+            <p>Y en la noche emanaba su brillo protector para nosotros, “Bondadosa Naturaleza”.</p>
+        </div>
+
+        <div class="row">
+            <div style="background-image: url(../images/naturaleza2.jpg);" id="naturaleza2">
+                <img src="../images/naturaleza8.jpg" alt="naturaleza8" id="naturaleza8">
+            </div>
+        </div>
+
+        <div class="row">
+            <div style="background-image: url(../images/naturaleza5.jpg);" id="naturaleza5" class="card-large">
+            </div>
+        </div>
+
+        <div class="row">
+            <div style="background-image: url(../images/naturaleza6.jpg);" id="naturaleza6" class="card-large">
+            </div>
+        </div>
+
+        <div class="row">
+            <div style="background-image: url(../images/naturaleza3.jpg);" id="naturaleza3" class="card-large">
+            </div>
+        </div>
+
+        <div class="row">
+            <div style="background-image: url(../images/naturaleza4.jpg);" id="naturaleza4" class="card-large">
+            </div>
+        </div>
+
+        <div class="row">
+            <div style="background-image: url(../images/naturaleza1.jpg);" id="naturaleza1" class="card-large">
+            </div>
+        </div>
+
+        <?php 
+            include '../includes/logo.php';
+            includePagination('/pages/home-tejidos.php', '/');
+        ?>
+    </div>
+</div>
 
 <?php include '../includes/footer.php'; ?>
